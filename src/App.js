05066eb1b596d7d20810315717cdceb12014f0eb
Lindsey Yucha <yuchalin@msu.edu>
2024-04-21 19:58:07 -0400

    seeing if git will work on new build

--- src/App.js
+++ src/App.js
@@ -1,23 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import RecipeSearch from './RecipeSearch.js';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container mx-auto">
+      <h1 className="text-3xl font-semibold text-center my-8">Recipe Search App</h1>
+      <RecipeSearch />
     </div>
   );
 }
